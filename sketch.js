364828a0a5603f433eb2a7db2efef1ab6bcd6cc8
darshagarwal <64535202+darshagarwal@users.noreply.github.com>
2020-07-06 22:32:31 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
-var paper;
+var paper,rope;
 var container,container2,container3,wall;
 var ground;
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+const Constraint = Matter.Constraint;
 const Render = Matter.Render;
 
 function setup() {
@@ -23,6 +24,7 @@ function setup() {
 	container = new Container1(1085,490,5,175);
 	container2 = new Container(1200,570,200,3000);
 	container3 = new Container1(1315,490,5,175);
+	rope = new Chain(paper.body,{x:150,y:200})
 	Engine.run(engine);
 }
 
@@ -36,24 +38,15 @@ function draw() {
   container.display();
   container2.display();
   wall.display();
+  rope.display();
   drawSprites();
 
 }
 
-function keyPressed(pos, xpos, ypos){
-	//  var pos = this.body.position;
-	  var boject= this.body;
-	  var xpos=xpos;
-	  var ypos=ypos;
-	  //console.log("darsh before "+ xpos);
-	 // console.log("darshbefore" + ypos);
-  
-  
-	  
-	  if(keyCode === UP_ARROW){
-		  console.log("darsh if arrow pressed "+ pos);
-	 
-			 Matter.Body.applyForce( paper.body,paper.position,{x:85,y:-85});
-		 }
-	 }
-	 
+	 function mouseDragged(){
+		Matter.Body.setPosition(paper.body,{x:mouseX,y:mouseY});
+	}
+	
+	function mouseReleased(){
+		chain.fly();
+	}
